ab7cf8ebf42f08dfcea92e5c52c1ca1070b38d06
Ali Umar <ali.umar.work@gmail.com>
2023-03-14 15:02:12 -0400

    move image on keypress

--- src/main.c
+++ src/main.c
@@ -1,52 +1,50 @@
 /*******************************************************************************************
  *  Name: Ali Umar
- *  Title: Mushroom Fighter 
- *  Description: A small fighting game arund the theme of mushrooms 
+ *  Title: Mushroom Fighter
+ *  Description: A small fighting game arund the theme of mushrooms
  ********************************************************************************************/
 
 #include <stdio.h>
 #include "raylib.h"
+#include "map_loader.h"
+
+#define SCREEN_WIDTH 900
+#define SCREEN_HEIGHT 400
+#define WINDOW_TITLE "Mushroom Fighter"
 
 int main(void)
 {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+  const int shroom_offset_x = 8;
+  const int shroom_offset_y = 64;
+  int shroom_x_pos = 0;
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+  SetTargetFPS(60);
 
-  InitWindow(screenWidth, screenHeight, "Mushroom Fighter");
-
-  SetTargetFPS(60); 
-
-  InitAudioDevice();
-  if (IsAudioDeviceReady())
-  {
-    printf("ready\n");
-  }
-
-  Wave sample = LoadWave("example.wav");
-  if (IsWaveReady(sample))
-  {
-    printf("ready\n");
-  }
-
-  Sound audio = LoadSoundFromWave(sample);
-  if (IsSoundReady(audio))
-  {
-    printf("ready\n");
-  }
-
-  PlaySound(audio);
+  Texture2D background = load_map();
+  Texture2D player = LoadTexture("assets\\players\\without background\\11.png");
 
   while (!WindowShouldClose())
   {
     BeginDrawing();
+    ClearBackground(WHITE);
+    // DrawTexture(background, 0, 0, WHITE);
+    DrawTextureEx(player, (Vector2){shroom_x_pos + shroom_offset_x, SCREEN_HEIGHT - shroom_offset_y}, 0, 0.1, WHITE);
 
-    ClearBackground(RAYWHITE);
+    if(IsKeyDown(KEY_D)) {
+      shroom_x_pos++;
+    }
 
-    DrawText("Hello World", 20, 20, 10, BLACK);
+    if(IsKeyDown(KEY_A)) {
+      if(shroom_x_pos > 0) {
+        shroom_x_pos--;
+      }
+    }
 
     EndDrawing();
   }
-  CloseWindow(); 
+
+  UnloadTexture(background);
+  CloseWindow();
 
   return 0;
 }

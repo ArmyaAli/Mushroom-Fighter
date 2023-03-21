6e498b466532523a5d92d72a57eb7ed196352acf
Ali Umar <ali.umar.work@gmail.com>
2023-03-21 09:57:14 -0400

    refactor using raygui

--- include/menu.h
+++ include/menu.h
@@ -1,9 +1,9 @@
 #ifndef MENU_H
 #define MENU_H
-#define TITLE MUSHROOM_FIGHTER
-
+#define MENU_TITLE "MUSHROOM FIGHTER"
+#include "raygui.h"
 #include "raylib.h"
-#include "asset_loader.h"
+#include <stdlib.h>
 
 const char* BTN_1_TEXT = "PLAY";
 const char* BTN_2_TEXT = "HELP";
@@ -11,13 +11,11 @@ const char* BTN_3_TEXT = "CREDITS";
 
 const int BTN_ANCHOR_Y = 100;
 
+Rectangle BACKGROUND = {50, 25, 300, 340};
+Rectangle BTN_1      = {100, 60, 200, 80};
+Rectangle BTN_2      = {100, 140 + 25, 200, 80};
+Rectangle BTN_3      = {100, 220 + 50, 200, 80};
 
-Rectangle BTN_1 = {100, 50, 200, 80};
-Rectangle BTN_2 = {100, 150, 200, 80};
-Rectangle BTN_3 = {100, 250, 200, 80};
-
-Color MENU_BTN1_COLOR = {255, 0, 0, 255};
-Color MENU_BTN2_COLOR = {255, 0, 0, 255};
-Color MENU_BTN3_COLOR = {255, 0, 0, 255};
+Color MENU_BACKGROUND_COLOR = {0, 255, 0, 125};
 
 #endif

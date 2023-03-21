5be89a89b871aa876adbba060437636db8534263
Ali Umar <ali.umar.work@gmail.com>
2023-03-21 09:56:08 -0400

    wobble_walk animation, completed state machine

--- include/animations.h
+++ include/animations.h
@@ -1,6 +1,36 @@
 #ifndef ANIMATIONS_H
 #define ANIMATIONS_H
+#include <stdlib.h>
 
-void wobble_walk(int* rotation);
+// BEGIN STATE MACHINE FRAMEWORK
+
+struct state_chain;
+
+typedef int state_fn(struct state_chain*);
+
+struct state_chain {
+  state_fn* next;
+  float* rotation;
+  float* x;
+  float* y;
+  float dt;
+};
+
+// END STATE MACHINE FRAMEWORK
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// BEGIN STATE MACHINE - WOBBLE_WALK
+// STATES
+// NOP - NO OPERATION (DO NOTHING)
+// RIGHT_STATE -> WOBBLE TO THE RIGHT
+// RIGHT_STATE -> WOBBLE TO THE LEFT 
+// RIGHT_STATE -> WOBBLE BACK TO THE RIGHT
+
+typedef enum { NOP, RIGHT_STATE, LEFT_STATE, END_STATE } WOBBLE_WALK_STATE;
+state_fn right, left, end;
+WOBBLE_WALK_STATE wobble_walk(float*, float, WOBBLE_WALK_STATE);
+
+// END STATE MACHINE - WOBBLE_WALK
 
 #endif

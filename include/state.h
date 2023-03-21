2a1a6609f9d4f2d38b8ac3d08cfd0a794d01f69c
Ali Umar <ali.umar.work@gmail.com>
2023-03-21 09:58:13 -0400

    add comments

--- include/state.h
+++ include/state.h
@@ -1,17 +1,21 @@
 #ifndef STATE_H
 #define STATE_H
 
+// NOTE
+// When modifying this enum ensure that menu.c extern anonymous enum is also updated.
+// You are also requried to update the state handler in main.c
 typedef enum GAME_STATE {
   BOOTSTRAP,
   MENU,
   HELP,
+  CREDITS,
   CHARACTER_SELECTION_MENU,
   GAME_IN_PROGRESS,
   GAME_PAUSED,
   GAME_OVER
 } GAME_STATE;
 
-GAME_STATE CURRENT_STATE = MENU;
+GAME_STATE CURRENT_STATE = GAME_IN_PROGRESS;
 
 extern GAME_STATE CURRENT_STATE;
 
